1002666e6e0fce15740157be5d0dccad2e0451a6
Christina Koch <christinakconnect@gmail.com>
2015-06-04 16:00:33 -0500

    changing index to name

--- stats.py
+++ stats.py
@@ -13,7 +13,7 @@ def mean(vals):
 def median(vals):
     vals.sort()
     z = len(vals)
-    index = z / 2
+    name = z / 2
     if z % 2 == 0:
        return mean([vals[index], vals[index - 1]])
     else:
